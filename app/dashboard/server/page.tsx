0c41f3a2c6987f0486bc9e762db2a50e8df3abbd
Khalil <101659794+KHLALA-Gh@users.noreply.github.com>
2023-08-18 22:26:12 +0100

    Update page.tsx

--- app/dashboard/server/page.tsx
+++ app/dashboard/server/page.tsx
@@ -11,7 +11,7 @@ export default function Page({ searchParams }: Props) {
       <div className="w-full h-full flex justify-center items-center flex-col">
         <Image src={"/img/rnd_rock.png"} alt="-_^" width={128} height={128} />
         <h1 className="text-center mt-5">
-          this page will be available in 1.0.0 RandomBot version
+          this page will be available in 1.1.0 RandomBot version
         </h1>
       </div>
     </>
